055fd3e0d573a5bc6751f7d05461076b0b8d25be
Alan <a33845637@gmail.com>
2025-07-23 09:43:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# OpenWrt DIY script part 2 (After Update feeds)
 
-# Modify default IP
+# 修改默认 IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改默认主题为 Argon（取消注释以启用）
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 修改 hostname
+# sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# ❗去除已有的 alist 和 tailscale 插件（如果之前添加过）
+rm -rf package/luci-app-alist package/alist
+rm -rf package/tailscale
+
+# ✅ 添加 Lucky 插件
+git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
+
+# ✅ 添加 ZeroTier 插件
+git clone https://github.com/mwarning/zerotier-openwrt package/zerotier
+git clone https://github.com/rufengsuixing/luci-app-zerotier package/luci-app-zerotier
